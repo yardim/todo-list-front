6992065852059f7b0ce648047427642348e0af6c
Dmytro Yaroshenko <dmytro.yaroshenko@dev-pro.net>
2018-06-15 18:22:03 +0300

    Development of password match validation

--- src/app/sign-up/sign-up.component.ts
+++ src/app/sign-up/sign-up.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormControl, Validators } from '@angular/forms';
+import { FormControl, FormGroup, FormBuilder, Validators } from '@angular/forms';
 
 import { matchPassword } from './validations';
 
@@ -11,22 +11,61 @@ import { matchPassword } from './validations';
 export class SignUpComponent implements OnInit {
   public nameControl: FormControl;
   public emailControl: FormControl;
+  public password: FormGroup;
   public pswdControl: FormControl;
   public pswdSecondControl: FormControl;
 
-  ngOnInit() {
+  constructor(private formBuilder: FormBuilder) {}
+
+  public ngOnInit() {
     this.nameControl = new FormControl('', [ Validators.required ]);
     this.emailControl = new FormControl('', [ Validators.required, Validators.email ]);
+
+    this.password = new FormGroup({
+      pswdControl: new FormControl('', [
+        Validators.minLength(6),
+        Validators.required
+      ]),
+      pswdSecondControl: new FormControl('', [
+        Validators.minLength(6),
+        Validators.required
+      ])
+    }, [
+        matchPassword
+    ]);
+
     // TODO: add match password validaion using form group
     // http://www.codiodes.com/match-password-validation/
-    this.pswdControl = new FormControl('', [
-      Validators.minLength(6),
-      Validators.required
-    ]);
-    this.pswdSecondControl = new FormControl('', [
-      Validators.minLength(6),
-      Validators.required,
-      matchPassword(this.pswdControl)
-    ]);
+    // this.pswdControl = new FormControl('', [
+    //   Validators.minLength(6),
+    //   Validators.required
+    // ]);
+    // this.pswdSecondControl = new FormControl('', [
+    //   Validators.required,
+    //   Validators.minLength(6),
+    //   matchPassword(this.pswdControl)
+    // ]);
+  }
+
+  public getErrorMessage(errors: any): string {
+    if (!errors) {
+      return '';
+    }
+
+    if (errors.required) {
+      return 'This field is required';
+    }
+
+    if (errors.email) {
+      return 'Email should be valid';
+    }
+
+    if (errors.minlength) {
+      return `This field should contain minimum ${errors.minlength.requiredLength} symbols`;
+    }
+
+    if (errors.matchPassword) {
+      return 'Passwords should match';
+    }
   }
 }
--- src/app/sign-up/validations.ts
+++ src/app/sign-up/validations.ts
@@ -1,18 +1,14 @@
-import { AbstractControl, FormControl } from '@angular/forms';
+import { FormGroup } from '@angular/forms';
 
-export function matchPassword(firstPswdControl: FormControl) {
-  return (currentPasswordField: AbstractControl): { [key: string]: any } | null => {
-    const currentPswd: string = currentPasswordField.value;
-    const firstPswd: string = firstPswdControl.value;
-    console.log(firstPswd);
-    console.log(currentPswd);
+export function matchPassword(password: FormGroup) {
+  const pswd: string = password.controls.pswdControl.value;
+  const pswdRepeat: string = password.controls.pswdSecondControl.value;
 
-    return firstPswd !== currentPswd
-      ? {
-          'matchPassword': {
-            value: 'Passwords does not match'
-          }
-        }
-      : null;
-  };
+  if (pswd !== pswdRepeat) {
+    return {
+      matchPassword: true
+    };
+  }
+
+  return null;
 }
